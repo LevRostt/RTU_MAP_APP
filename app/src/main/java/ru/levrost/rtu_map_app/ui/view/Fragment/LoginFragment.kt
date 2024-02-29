f10cbb92bdcb682cd628c0aab930cc18ac009d32
LevRost <levitskiy.r.l@edu.mirea.ru>
2024-03-01 00:00:03 +0300

    add mapKit, based Navigation

--- app/src/main/java/ru/levrost/rtu_map_app/ui/view/Fragment/LoginFragment.kt
+++ app/src/main/java/ru/levrost/rtu_map_app/ui/view/Fragment/LoginFragment.kt
@@ -1,11 +1,16 @@
 package ru.levrost.rtu_map_app.ui.view.Fragment
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
+import ru.levrost.rtu_map_app.R
 import ru.levrost.rtu_map_app.databinding.LoginFragmentBinding
+
 
 class LoginFragment: Fragment() {
     private lateinit var mBinding: LoginFragmentBinding
@@ -16,7 +21,13 @@ class LoginFragment: Fragment() {
     ): View {
         mBinding = LoginFragmentBinding.inflate(inflater, container, false)
 
+        mBinding.btnReg.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
 
+        mBinding.singInAsGuest.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
 
 
         return mBinding.root
